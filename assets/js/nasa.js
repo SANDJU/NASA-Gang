f468912246cf0c8f541dc555174c5546da28f9bd
Nick Reisner <nreisne@wgu.edu>
2021-04-26 20:20:51 -0600

    Added Js for rover photos

--- assets/js/nasa.js
+++ assets/js/nasa.js
@@ -1,1 +1,23 @@
+var Curiosity = document.getElementById("Curriosity")
+var Opportunity = document.getElementById("Opportunity")
+var Spirit = document.getElementById("Spirit")
+var Rovers = document.getElementById("Rovers")
 
+
+var apiURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=PUxgro2fgT0RlNQ3CSy2X8Zxk0hbwxZoWFR2UPh3"
+
+					var apiKey = 'PUxgro2fgT0RlNQ3CSy2X8Zxk0hbwxZoWFR2UPh3'; 
+
+					var request = new XMLHttpRequest(); 
+					request.open('GET', exampleURL + '&api_key=' + apiKey, true);
+
+					request.addEventListener('load',function(){
+
+					if(request.status >= 200 && request.status < 400){
+					var response = JSON.parse(request.responseText);
+					console.log(response);
+					} 
+					else {
+					     console.log("Error in network request: " + request.statusText);
+					}});
+					request.send(null);
